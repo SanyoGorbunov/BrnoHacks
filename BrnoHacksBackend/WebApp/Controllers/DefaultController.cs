44f7bfb4c3b0d7301b74ed632ce71c81620b0d3d
Alex Gorbunov <sanyo.gorbunov@gmail.com>
2017-05-26 23:31:45 +0200

    Return json from controller.

---
+++ BrnoHacksBackend/WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace WebApp.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public IHttpActionResult GetProduct(int id)
+        {
+            return Ok(new { myId = id});
+        }
+    }
+}
